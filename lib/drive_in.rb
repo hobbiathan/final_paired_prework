197109828c5740e052f28a7756300e2d4fde1323
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-10 14:17:20 -0700

    Create #vehicles_with_passengers method

--- lib/drive_in.rb
+++ lib/drive_in.rb
@@ -7,5 +7,14 @@ class DriveIn
     @vehicles_parked = []
   end
 
+  def park_vehicle(vehicle_obj)
+    @vehicles_parked << vehicle_obj
+  end
 
+  def vehicles_with_passengers
+    total = vehicles_parked.select do |vehicle|
+      # require 'pry'; binding.pry
+      vehicle.passengers.size > 1
+    end
+  end
 end
